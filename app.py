dbe10e5b54442ccf7997733739bd7b71a663c16a
Manya Singh <114657669+Manya-singh2001@users.noreply.github.com>
2026-03-14 01:31:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,22 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
+
+from datetime import datetime
+
+
+import asyncio
+
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
